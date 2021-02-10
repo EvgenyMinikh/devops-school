3c30f57e318d12bba8e0744e007a8eb0fe9d75b7
Evgenii Minikh <evgeny.minih@gmail.com>
2021-02-10 18:15:22 +0300

    [Change] Added user parameters for action button. Text is written into artifact

--- .github/actions/action_1.2/entrypoint.sh
+++ .github/actions/action_1.2/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -l
 
 echo "EXECUTING PYTHON SCRIPT..."
-python3 /main.py
+python3 /main.py ${INPUT_FIRSTPARAM} ${INPUT_SECONDPARAM}
 
 echo "ls -lah"
 ls -lah
--- scripts/main.py
+++ scripts/main.py
@@ -1,10 +1,16 @@
 from datetime import datetime
+import sys
 
 print("PRINT FROM PYTHON: ACTION 1.2 - STARTED")
+
+first_arg = sys.argv[1]
+second_arg = sys.argv[2]
 
 timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
 with open('log.md', 'w') as f:
     f.write(f'# {timestamp}')
+    f.write("\n1st arg: {}\n".format(first_arg))
+    f.write("2nd arg: {}\n".format(second_arg))
     
 print("PRINT FROM PYTHON: ACTION 1.2 - COMPLETED")
